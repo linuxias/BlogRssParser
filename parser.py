cdd36b181993192a3907fd233aabbcca1dee0ad1
Seungha Son <linuxias@gmail.com>
2018-11-17 17:49:21 +0900

    Add slacker init code
    Signed-off-by: Seungha Son <linuxias@gmail.com>

--- parser.py
+++ parser.py
@@ -1,6 +1,8 @@
 import feedparser
+from slacker import Slacker
 
 url_file = 'url_list'
+token_file = 'token_file'
 
 def get_url_list():
     url_list = []
@@ -10,10 +12,25 @@ def get_url_list():
 
     return url_list
 
+def get_token():
+    token = ''
+    with open(token_file) as f:
+        token = f.readline()
+    return token.strip()
+
 def main():
     url_list = get_url_list()
     for url in url_list:
         f = feedparser.parse(url)
+        for var in f:
+            print(var)
+
+    token = get_token()
+    try:
+        slack = Slacker(token)
+        slack.chat.post_message('#general', 'Hello')
+    except Exception as e:
+        print('Wrong token : ', token)
 
 if __name__=="__main__":
     main()
