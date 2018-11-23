bfe7d87e47ba9ba7daf3a145d6e2ee469327db4e
Seungha Son <linuxias@gmail.com>
2018-11-23 23:41:22 +0900

    Add attachments argument in post_message
    Signed-off-by: Seungha Son <linuxias@gmail.com>

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import feedparser
 from slacker import Slacker
+from query import RssQuery
 
 url_file = 'url_list'
 token_file = 'token_file'
@@ -9,7 +10,6 @@ def get_url_list():
     with open(url_file) as f:
         for data in f:
             url_list.append(data)
-
     return url_list
 
 def get_token():
@@ -18,19 +18,24 @@ def get_token():
         token = f.readline()
     return token.strip()
 
+def slacker_post():
+    token = get_token()
+    attatchment = [{
+        "title" : "title test",
+        "title_link" : "https://sonseungha.tistory.com"
+    }]
+    slack = Slacker(token)
+    slack.chat.post_message(channel='#general', text='New article', attachments = attatchment)
+
 def main():
     url_list = get_url_list()
+
     for url in url_list:
         f = feedparser.parse(url)
         for var in f:
             print(var)
 
-    token = get_token()
-    try:
-        slack = Slacker(token)
-        slack.chat.post_message('#general', 'Hello')
-    except Exception as e:
-        print('Wrong token : ', token)
+    slacker_post()
 
 if __name__=="__main__":
     main()
