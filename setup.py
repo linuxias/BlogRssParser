7ae6fe6bfb4316f15edbffbbfab2c9fe44b03486
Seungha Son <seungha.son@samsung.com>
2020-01-20 19:17:14 +0900

    Prepare packaging architecture

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+REQUIRES = []
+with open('requirements.txt') as f:
+    for line in f:
+        REQUIRES.append(line)
+
+setup(
+    name='blogrssparser',
+    version='0.1',
+    description="slack chatbot to notify articles",
+    author="Seungha Son",
+    author_email="linuxias@gmail.com",
+    license="MIT",
+    url="https://github.com/linuxias/blogrssparser",
+    keywords=["RSS", "Chatbot", "Parser"],
+    install_requires=REQUIRES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
